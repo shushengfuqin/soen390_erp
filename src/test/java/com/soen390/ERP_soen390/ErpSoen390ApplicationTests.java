a3cdbb15a54044855dfa37833679462bb8624ab4
Ruochen Jia <ruocjia@gmail.com>
2021-01-19 14:23:42 -0500

    basic spring server
    basic spring server with maven dependencies. 
    Still working on sql database and restful api

---
+++ src/test/java/com/soen390/ERP_soen390/ErpSoen390ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soen390.ERP_soen390;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpSoen390ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
